db6cf32e91d88546db59c9f0b47f0f85c7213cc1
72OTA <f.andradevalenzuela@gmail.com>
2017-11-20 12:00:28 -0400

    Nuevos ejercicios.

---
+++ EliminarPrimerayUltima.js
@@ -0,0 +1,5 @@
+function removerLetra(str){
+ //You got this!
+ // remueve la primera y la ultima letra de una palabra
+return str.slice(1,-1);
+};
---
+++ PasarBooleano.js
@@ -0,0 +1,3 @@
+function booleanoPalabra( bool ){
+  return bool ? 'Si':'No';
+}
